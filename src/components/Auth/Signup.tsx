f25be5b3baba83773f945bd073985c6b99c182e7
dhanush901 <reddybhdhanush@gmail.com>
2024-02-22 11:57:17 +0530

    signup is working properly

--- src/components/Auth/Signup.tsx
+++ src/components/Auth/Signup.tsx
@@ -1,44 +1,56 @@
-import * as React from "react";
-import Avatar from "@mui/material/Avatar";
-import Button from "@mui/material/Button";
-import CssBaseline from "@mui/material/CssBaseline";
-import TextField from "@mui/material/TextField";
-import Grid from "@mui/material/Grid";
-import Box from "@mui/material/Box";
-import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
-import Typography from "@mui/material/Typography";
-import Container from "@mui/material/Container";
-import { createTheme, ThemeProvider } from "@mui/material/styles";
-import { UserContextApi } from "../../context/AuthContext";
-import { useContext, useState } from "react";
-import { PayloadProps } from "../../types/PayloadType";
-import { NavLink } from "react-router-dom";
-import { useNavigate } from "react-router-dom";
+import * as React from 'react';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { UserContextApi } from '../../context/AuthContext';
+import { useContext,useState } from 'react';
+import { PayloadProps } from '../../types/PayloadType';
+import { NavLink } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
+
+
+
 
 
 const defaultTheme = createTheme();
 
 export default function Signup() {
-  const navigate = useNavigate();
-  const contextValue = useContext(UserContextApi);
-  const signup = contextValue?.signup;
 
-  const [formData, setFormData] = useState<PayloadProps>({
-    firstName: "",
-    lastName: "",
-    email: "",
-    password: "",
-  });
+let navigate = useNavigate();
+    const contextValue=useContext(UserContextApi)
+    const signup=contextValue?.signup
+    
+    
+    const [formData, setFormData] = useState<PayloadProps>({
+      firstName: "",
+      lastName: "",
+      email: "",
+      password: "",
+        });
 
-  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    if (signup) {
-      signup(formData);
-      navigate("/login");
-    } else {
-      navigate("/");
-    }
-  };
+
+
+        const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+          event.preventDefault();
+        
+         
+         
+        
+          if (signup) {
+            signup(formData);
+            navigate("/");
+            window.location.reload();
+          }
+        };
+        
+
 
   const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     const { name, value } = event.target;
@@ -54,95 +66,90 @@ export default function Signup() {
         <CssBaseline />
         <Box
           sx={{
-            marginTop: 15,
-            display: "flex",
-            flexDirection: "column",
-            alignItems: "center",
+            marginTop: 8,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
           }}
         >
-          <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
+          <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
             <LockOutlinedIcon />
           </Avatar>
           <Typography component="h1" variant="h5">
             Sign up
           </Typography>
-          <form>
-            <Box
-              component="form"
-              onSubmit={handleSubmit}
-              sx={{ mt: 3 }}
+          <Box component="form" onSubmit={handleSubmit} sx={{ mt: 3 }}>
+            <Grid container spacing={2}>
+              <Grid item xs={12} sm={6}>
+                <TextField
+                  autoComplete="given-name"
+                  name="firstName"
+                  required
+                  fullWidth
+                  id="firstName"
+                  value={formData.firstName}
+                  onChange={handleInputChange}
+                  label="First Name"
+                  
+                  autoFocus
+                />
+              </Grid>
+              <Grid item xs={12} sm={6}>
+                <TextField
+                  required
+                  fullWidth
+                  id="lastName"
+                  label="Last Name"
+                  name="lastName"
+                  value={formData.lastName}
+                  onChange={handleInputChange}
+                  autoComplete="family-name"
+                />
+              </Grid>
+              <Grid item xs={12} >
+                <TextField
+                  required
+                  fullWidth
+                  id="email"
+                  label="Email Address"
+                  name="email"
+                  type='email'
+                  value={formData.email}
+                  onChange={handleInputChange}
+                  autoComplete="email"
+                />
+              </Grid>
+              <Grid item xs={12}> 
+                <TextField
+                  required
+                  fullWidth
+                  name="password"
+                  label="Password"
+                  type="password"
+                  id="password"
+                  value={formData.password}
+                  onChange={handleInputChange}
+                  autoComplete="new-password"
+                />
+              </Grid>
+              
+            </Grid>
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
             >
-              <Grid container spacing={2}>
-                <Grid item xs={12} sm={6}>
-                  <TextField
-                    autoComplete="given-name"
-                    name="firstName"
-                    required
-                    fullWidth
-                    id="firstName"
-                    value={formData.firstName}
-                    onChange={handleInputChange}
-                    label="First Name"
-                    autoFocus
-                  />
-                </Grid>
-                <Grid item xs={12} sm={6}>
-                  <TextField
-                    required
-                    fullWidth
-                    id="lastName"
-                    label="Last Name"
-                    name="lastName"
-                    value={formData.lastName}
-                    onChange={handleInputChange}
-                    autoComplete="family-name"
-                  />
-                </Grid>
-                <Grid item xs={12}>
-                  <TextField
-                    required
-                    fullWidth
-                    type="email"
-                    id="email"
-                    label="Email Address"
-                    name="email"
-                    value={formData.email}
-                    onChange={handleInputChange}
-                    autoComplete="email"
-                  />
-                </Grid>
-                <Grid item xs={12}>
-                  <TextField
-                    required
-                    fullWidth
-                    name="password"
-                    label="Password"
-                    type="password"
-                    id="password"
-                    value={formData.password}
-                    onChange={handleInputChange}
-                    autoComplete="new-password"
-                  />
-                </Grid>
+              Sign Up
+            </Button>
+            <Grid container justifyContent="center">
+            <Grid item >
+              <NavLink to="/">Already have an account? Sign In</NavLink>
               </Grid>
-              <Button
-                type="submit"
-                fullWidth
-                variant="contained"
-                sx={{ mt: 3, mb: 2 }}
-              >
-                Sign Up
-              </Button>
-              <Grid container justifyContent="center">
-                <Grid item>
-                  <NavLink to="/login">
-                    Already have an account? Sign In
-                  </NavLink>
-                </Grid>
-              </Grid>
-            </Box>
-          </form>
+            </Grid>
+          </Box>
         </Box>
+   
       </Container>
     </ThemeProvider>
   );
