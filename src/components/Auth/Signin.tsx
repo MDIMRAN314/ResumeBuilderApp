89e9d32505b5e8a88390a5d8c86c84dc66de22ac
dhanush901 <reddybhdhanush@gmail.com>
2024-02-23 10:47:25 +0530

    signin page is modified

--- src/components/Auth/Signin.tsx
+++ src/components/Auth/Signin.tsx
@@ -1,7 +1,7 @@
 import * as React from "react";
-import Avatar from "@mui/material/Avatar";
+
 import Button from "@mui/material/Button";
-import CssBaseline from "@mui/material/CssBaseline";
+
 import IconButton from "@mui/material/IconButton";
 import OutlinedInput from "@mui/material/OutlinedInput";
 import InputLabel from "@mui/material/InputLabel";
@@ -12,13 +12,16 @@ import VisibilityOff from "@mui/icons-material/VisibilityOff";
 import Paper from "@mui/material/Paper";
 import Box from "@mui/material/Box";
 import Grid from "@mui/material/Grid";
-import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
+
 import Typography from "@mui/material/Typography";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 import { UserContextApi } from "../../context/AuthContext";
 import { UsersContextApi } from "../../context/UsersContext";
 import { useContext, useState } from "react";
 import { NavLink, useNavigate } from "react-router-dom";
+import SwipeableTextMobileStepper from "../../carsoul/Carosul";
+import { Avatar, Container } from "@mui/material";
+import logo from "../../assets/reslogo2.jpg";
 
 const defaultTheme = createTheme();
 export default function Signin() {
@@ -77,104 +80,151 @@ export default function Signin() {
   };
 
   return (
-    <ThemeProvider theme={defaultTheme}>
-      <Grid container component="main" sx={{ height: "100vh" }}>
-        <CssBaseline />
+    <>
+      <ThemeProvider theme={defaultTheme}>
         <Grid
-          item
-          xs={false}
-          sm={4}
-          md={7}
-          sx={{
-            backgroundImage:
-              "url(https://source.unsplash.com/random?wallpapers)",
-            backgroundRepeat: "no-repeat",
-            backgroundColor: (t) =>
-              t.palette.mode === "light"
-                ? t.palette.grey[50]
-                : t.palette.grey[900],
-            backgroundSize: "cover",
-            backgroundPosition: "center",
-          }}
-        />
-        <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
-          <Box
+          container
+          component="main"
+          sx={{ height: "100vh", width: "100%" }}
+        >
+          <Grid
+            item
+            md={6}
             sx={{
-              my: 15,
-              mx: 4,
               display: "flex",
-              flexDirection: "column",
+              justifyContent: "center",
               alignItems: "center",
             }}
           >
-            <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
-              <LockOutlinedIcon />
-            </Avatar>
-            <Typography component="h1" variant="h5">
-              Sign in
-            </Typography>
+            <Container sx={{ padding: "0px !important" }}>
+              <SwipeableTextMobileStepper />
+            </Container>
+          </Grid>
+          <Grid
+            item
+            xs={12}
+            sm={8}
+            md={6}
+            
+            component={Paper}
+            elevation={6}
+            square
 
-            <Box component="form" onSubmit={handleSubmit} sx={{ mt: 1 }}>
-              <FormControl variant="outlined" fullWidth margin="normal">
-                <InputLabel htmlFor="outlined-adornment-email">
-                  Email Address
-                </InputLabel>
-                <OutlinedInput
-                  id="outlined-adornment-email"
-                  type="email"
-                  value={email}
-                  onChange={handleEmailChange}
-                  label="Email Address"
-                  autoFocus
-                />
-              </FormControl>
-
-              <FormControl variant="outlined" fullWidth margin="normal">
-                <InputLabel htmlFor="outlined-adornment-password">
-                  Password
-                </InputLabel>
-                <OutlinedInput
-                  id="outlined-adornment-password"
-                  type={showPassword ? "text" : "password"}
-                  value={password}
-                  onChange={handlePasswordChange}
-                  endAdornment={
-                    <InputAdornment position="end">
-                      <IconButton
-                        aria-label="toggle password visibility"
-                        onClick={handleClickShowPassword}
-                        onMouseDown={handleMouseDownPassword}
-                        edge="end"
-                      >
-                        {showPassword ? <VisibilityOff /> : <Visibility />}
-                      </IconButton>
-                    </InputAdornment>
-                  }
-                  label="Password"
-                />
-              </FormControl>
-
-              <Button
-                type="submit"
-                fullWidth
-                variant="contained"
-                sx={{ mt: 3, mb: 2 }}
+          >
+            <Box
+               
+              sx={{
+                mt:10,
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                }}
+            >
+              
+              <Box
+                sx={{
+               
+                  height: "100%",
+                  maxWidth: "50%",
+                  display: "flex",
+                  justifyContent: "center",
+                  alignItems: "center",
+                  
+                }}
               >
-                Sign In
-              </Button>
-
-              <Grid
-                item
-                display="flex"
-                alignItems="center"
-                justifyContent="center"
-              >
-                <NavLink to="/signup">Don't have an account? Sign Up</NavLink>
-              </Grid>
+                <img
+                  src={logo}
+                  alt="Logo"
+                  style={{
+                    width: "100%",
+                    height: "100%",
+                    objectFit: "cover",
+                  }}
+                />
+              </Box>
             </Box>
-          </Box>
+            <Box
+              sx={{
+                my: 10,
+                mx: 7,
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+              }}
+            >
+              <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
+              
+              </Avatar>
+              <Typography
+                component="h1"
+                variant="h5"
+                style={{ textTransform: "capitalize" }}
+              >
+                Sign in
+              </Typography>
+
+              <Box component="form" onSubmit={handleSubmit} sx={{ mt: 1 }}>
+                <FormControl variant="outlined" fullWidth margin="normal">
+                  <InputLabel htmlFor="outlined-adornment-email">
+                    Email Address
+                  </InputLabel>
+                  <OutlinedInput
+                    id="outlined-adornment-email"
+                    type="email"
+                    value={email}
+                    onChange={handleEmailChange}
+                    label="Email Address"
+                    autoFocus
+                  />
+                </FormControl>
+
+                <FormControl variant="outlined" fullWidth margin="normal">
+                  <InputLabel htmlFor="outlined-adornment-password">
+                    Password
+                  </InputLabel>
+                  <OutlinedInput
+                    id="outlined-adornment-password"
+                    type={showPassword ? "text" : "password"}
+                    value={password}
+                    onChange={handlePasswordChange}
+                    endAdornment={
+                      <InputAdornment position="end">
+                        <IconButton
+                          aria-label="toggle password visibility"
+                          onClick={handleClickShowPassword}
+                          onMouseDown={handleMouseDownPassword}
+                          edge="end"
+                        >
+                          {showPassword ? <VisibilityOff /> : <Visibility />}
+                        </IconButton>
+                      </InputAdornment>
+                    }
+                    label="Password"
+                  />
+                </FormControl>
+
+                <Button
+                  type="submit"
+                  fullWidth
+                  variant="contained"
+                  sx={{ mt: 3, mb: 2 }}
+                >
+                  Sign In
+                </Button>
+
+                <Grid
+                  item
+                  display="flex"
+                  alignItems="center"
+                  justifyContent="center"
+                >
+                  <NavLink to="/signup">Don't have an account? Sign Up</NavLink>
+                </Grid>
+              </Box>
+            </Box>
+          </Grid>
         </Grid>
-      </Grid>
-    </ThemeProvider>
+      </ThemeProvider>
+    </>
   );
 }
--- src/components/Auth/Signup.tsx
+++ src/components/Auth/Signup.tsx
@@ -1,56 +1,45 @@
-import * as React from 'react';
-import Avatar from '@mui/material/Avatar';
-import Button from '@mui/material/Button';
-import CssBaseline from '@mui/material/CssBaseline';
-import TextField from '@mui/material/TextField';
-import Grid from '@mui/material/Grid';
-import Box from '@mui/material/Box';
-import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
-import Typography from '@mui/material/Typography';
-import Container from '@mui/material/Container';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
-import { UserContextApi } from '../../context/AuthContext';
-import { useContext,useState } from 'react';
-import { PayloadProps } from '../../types/PayloadType';
-import { NavLink } from 'react-router-dom';
-import { useNavigate } from 'react-router-dom';
-
-
-
+import * as React from "react";
+import Avatar from "@mui/material/Avatar";
+import Button from "@mui/material/Button";
+import Paper from "@mui/material/Paper";
+import TextField from "@mui/material/TextField";
+import Grid from "@mui/material/Grid";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import { UserContextApi } from "../../context/AuthContext";
+import { useContext, useState } from "react";
+import { PayloadProps } from "../../types/PayloadType";
+import { NavLink } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import SwipeableTextMobileStepper from "../../carsoul/Carosul";
+import logo from "../../assets/reslogo2.jpg";
 
 
 const defaultTheme = createTheme();
 
 export default function Signup() {
+  let navigate = useNavigate();
+  const contextValue = useContext(UserContextApi);
+  const signup = contextValue?.signup;
 
-let navigate = useNavigate();
-    const contextValue=useContext(UserContextApi)
-    const signup=contextValue?.signup
-    
-    
-    const [formData, setFormData] = useState<PayloadProps>({
-      firstName: "",
-      lastName: "",
-      email: "",
-      password: "",
-        });
+  const [formData, setFormData] = useState<PayloadProps>({
+    firstName: "",
+    lastName: "",
+    email: "",
+    password: "",
+  });
 
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
 
-
-        const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-          event.preventDefault();
-        
-         
-         
-        
-          if (signup) {
-            signup(formData);
-            navigate("/");
-            window.location.reload();
-          }
-        };
-        
-
+    if (signup) {
+      signup(formData);
+      navigate("/");
+      window.location.reload();
+    }
+  };
 
   const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     const { name, value } = event.target;
@@ -62,95 +51,143 @@ let navigate = useNavigate();
 
   return (
     <ThemeProvider theme={defaultTheme}>
-      <Container component="main" maxWidth="xs">
-        <CssBaseline />
-        <Box
+      <Grid container component="main" sx={{ height: "100vh", width: "100%" }}>
+        <Grid
+          item
+          md={6}
           sx={{
-            marginTop: 8,
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center',
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
           }}
         >
-          <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
-            <LockOutlinedIcon />
-          </Avatar>
-          <Typography component="h1" variant="h5">
-            Sign up
-          </Typography>
-          <Box component="form" onSubmit={handleSubmit} sx={{ mt: 3 }}>
-            <Grid container spacing={2}>
-              <Grid item xs={12} sm={6}>
-                <TextField
-                  autoComplete="given-name"
-                  name="firstName"
-                  required
-                  fullWidth
-                  id="firstName"
-                  value={formData.firstName}
-                  onChange={handleInputChange}
-                  label="First Name"
-                  
-                  autoFocus
+          <Container sx={{ padding: "0px !important" }}>
+            <SwipeableTextMobileStepper />
+          </Container>
+        </Grid>
+
+        <Grid 
+        item xs={12} 
+        sm={8} 
+        md={6} 
+        component={Paper}
+        elevation={6} 
+        square>
+          <Box
+            sx={{
+              marginTop: 8,
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+               
+            }}
+          >
+            <Box
+                sx={{
+                  height: "100%",
+                  maxWidth: "50%",
+                  display: "flex",
+                  justifyContent: "center",
+                  alignItems: "center",
+                }}
+              >
+                <img
+                  src={logo}
+                  alt="Logo"
+                  style={{
+                    width: "100%",
+                    height: "100%",
+                    objectFit: "cover",
+                  }}
                 />
-              </Grid>
-              <Grid item xs={12} sm={6}>
-                <TextField
-                  required
-                  fullWidth
-                  id="lastName"
-                  label="Last Name"
-                  name="lastName"
-                  value={formData.lastName}
-                  onChange={handleInputChange}
-                  autoComplete="family-name"
-                />
-              </Grid>
-              <Grid item xs={12} >
-                <TextField
-                  required
-                  fullWidth
-                  id="email"
-                  label="Email Address"
-                  name="email"
-                  type='email'
-                  value={formData.email}
-                  onChange={handleInputChange}
-                  autoComplete="email"
-                />
-              </Grid>
-              <Grid item xs={12}> 
-                <TextField
-                  required
-                  fullWidth
-                  name="password"
-                  label="Password"
-                  type="password"
-                  id="password"
-                  value={formData.password}
-                  onChange={handleInputChange}
-                  autoComplete="new-password"
-                />
-              </Grid>
-              
-            </Grid>
-            <Button
-              type="submit"
-              fullWidth
-              variant="contained"
-              sx={{ mt: 3, mb: 2 }}
+              </Box>
+            </Box>
+            <Box
+              sx={{
+                my: 10,
+                mx: 7,
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+              }}
             >
-              Sign Up
-            </Button>
-            <Grid container justifyContent="center">
-            <Grid item >
-              <NavLink to="/">Already have an account? Sign In</NavLink>
+            <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
+              
+            </Avatar>
+            <Typography component="h1" variant="h5" style={{ textTransform: "capitalize" }}>
+              Sign up
+            </Typography>
+            <Box component="form" onSubmit={handleSubmit} sx={{ mt: 3 }}>
+              <Grid container spacing={2}>
+                <Grid item xs={12} sm={6}>
+                  <TextField
+                    autoComplete="given-name"
+                    name="firstName"
+                    required
+                    fullWidth
+                    id="firstName"
+                    value={formData.firstName}
+                    onChange={handleInputChange}
+                    label="First Name"
+                    autoFocus
+                  />
+                </Grid>
+                <Grid item xs={12} sm={6}>
+                  <TextField
+                    required
+                    fullWidth
+                    id="lastName"
+                    label="Last Name"
+                    name="lastName"
+                    value={formData.lastName}
+                    onChange={handleInputChange}
+                    autoComplete="family-name"
+                  />
+                </Grid>
+                <Grid item xs={12}>
+                  <TextField
+                    required
+                    fullWidth
+                    id="email"
+                    label="Email Address"
+                    name="email"
+                    type="email"
+                    value={formData.email}
+                    onChange={handleInputChange}
+                    autoComplete="email"
+                  />
+                </Grid>
+                <Grid item xs={12}>
+                  <TextField
+                    required
+                    fullWidth
+                    name="password"
+                    label="Password"
+                    type="password"
+                    id="password"
+                    value={formData.password}
+                    onChange={handleInputChange}
+                    autoComplete="new-password"
+                  />
+                </Grid>
               </Grid>
-            </Grid>
-          </Box>
-        </Box>
-   
-      </Container>
+              <Button
+                type="submit"
+                fullWidth
+                variant="contained"
+                sx={{ mt: 3, mb: 2 }}
+              >
+                Sign Up
+              </Button>
+              <Grid container justifyContent="center">
+                <Grid item>
+                  <NavLink to="/login">Already have an account? Sign In</NavLink>
+                </Grid>
+              </Grid>
+            </Box>
+            </Box>
+        </Grid>
+      </Grid>
     </ThemeProvider>
   );
 }
